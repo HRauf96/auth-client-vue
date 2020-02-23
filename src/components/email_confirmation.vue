5b2298535fbb288c4e477c79caa5d0de9793065b
Habibullah Rauf <habrauf@hotmail.com>
2020-02-23 16:44:45 +0000

    New pages added

---
+++ src/components/email_confirmation.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <h1 class="header">{{ msg }}</h1>
+    <div id="root" class="container">
+        <p>Your Email is confirmed, click <a href="">here</a> to go to your profile</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Email Confirmed'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1.header{
+  font-weight: normal;
+}
+</style>
---
+++ src/components/re-ser-password.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="hello">
+    <h1 class="header">{{ msg }}</h1>
+    <div id="root" class="container">
+
+<form class="form-forgot-password">
+      <div class="form-container">
+
+        <label for="email"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="email" required>
+        <input type="password" placeholder="Repeat Password" name="psw" required>
+
+      <div class="clearfix">
+      <button type="submit" class="sumbit-button">Reset</button>
+    </div>
+  </div>
+</form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Re-set Password'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1.header{
+  font-weight: normal;
+}
+
+input[type=password] {
+  width: 100%;
+  padding: 10px;
+  margin: 5px 0 22px 0;
+  display: inline-block;
+  border: none;
+  background: #f1f1f1;
+}
+
+input[type=password]:focus {
+  background-color: #ddd;
+  outline: none;
+}
+
+.submit-button {
+  background-color: #4CAF50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+  opacity: 0.9;
+}
+
+.submit-button:hover {
+  opacity:1;
+}
+
+.form-container {
+  padding: 16px;
+}
+
+.form-forgot-password{
+border:1px solid #ccc;
+width: 50%;
+margin: auto;
+}
+
+.clearfix::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+* {box-sizing: border-box}
+</style>
